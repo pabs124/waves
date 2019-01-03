23b06df23c3aeb2c3d7ba87c0eb9f27095c9300c
Sahil Pabby <pabs124@gmail.com>
2019-01-03 13:28:45 -0500

    server finished

--- server/server.js
+++ server/server.js
@@ -17,14 +17,17 @@ app.use(cookieParser())
 
 const {User} = require('./models/user') 
 const {Brand} = require('./models/brand')
+const {Wood} = require('./models/wood')
+const {Product} = require('./models/product')
 
 // Middlewares
 
 const {auth} = require('./middleware/auth')
+const {admin} = require('./middleware/admin')
 
 //Brand
 
-app.post('/api/product/brand', auth, (req, res) => {
+app.post('/api/product/brand', auth,admin, (req, res) => {
     const brand = new Brand(req.body)
 
     brand.save((err, doc) => {
@@ -33,6 +36,84 @@ app.post('/api/product/brand', auth, (req, res) => {
             success:true,
             brand: doc
         })
+    })
+})
+
+app.get('/api/product/get_brands', (req, res) => {
+    Brand.find({}, (err, brands)=> {
+        if(err) return res.status(400).send(err)
+        res.status(200).send(brands)
+    })
+})
+
+//Woods
+
+app.post('/api/product/wood', auth, admin, (req, res) => {
+    const wood = new Wood(req.body)
+
+    wood.save((err, doc)=> {
+        if (err) return res.json({success: false, err})
+        res.status(200).json({
+            success: true,
+            wood: doc
+        })
+    })
+})
+
+app.get('/api/product/get_wood', (req, res) => {
+    Wood.find({}, (err, woods)=> {
+        if(err) return res.status(400).send(err)
+        res.status(200).send(woods)
+    })
+})
+
+// Products
+
+app.post('/api/product/articles',auth, admin, (req, res)=> {
+    const product = new Product(req.body)
+    product.save((err, doc) => {
+        if (err) return res.json({success: false, err})
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
+    })
+})
+
+app.get('/api/product/articles_by_id', (req, res) => {
+    let type = req.query.type
+    let items = req.query.id
+
+    if(type === 'array'){
+        let ids = req.query.id.split(',')
+        items = []
+        items = ids.map(item => {
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+    Product.
+    find({ '_id': {$in:items}}).
+    populate('brand').
+    populate('wood').
+    exec((err, docs)=> {
+        return res.status(200).send(docs)
+    })
+})
+
+app.get('/api/product/articles', (req, res)=> {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id"
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100
+    
+    Product.
+    find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy, order]]).
+    limit(limit).
+    exec((err, articles)=> {
+        if(err) return res.status(400).send(err)
+        res.send(articles)
     })
 })
 
